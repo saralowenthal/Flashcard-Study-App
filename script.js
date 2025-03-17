38012f1881d82770fdf3463aeadc2c5725d53472
tmeth <methtoby@gmail.com>
2025-03-17 13:15:14 -0400

    updating jsFunctions

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ document.addEventListener("DOMContentLoaded", () => {
     console.log("JavaScript is loaded and ready!");
 });
 
-const cards = require('./flashcards'); //json file of cards
+const flashcards = require('./flashcards'); //json file of cards
 
 
 //randomly shuffle cards and store in new array
-function shuffleCards(flashcards) {
-    let shuffledArray = [...flashcards]; //new array with shuffled cards
+function shuffleCards(cards) {
+    let shuffledArray = [...cards]; //new array with shuffled cards
 
     for (let i = shuffledArray.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
@@ -17,19 +17,21 @@ function shuffleCards(flashcards) {
     }
     return shuffledArray;
     }
-const gameCards=shuffleCards(); //shuffles all cards and stores in gameCards
-document.getElementById("card-front").innerHTML = gameCards[0].front;//displays the 1st card in card-front
+
+const gameCards=shuffleCards(flashcards); //shuffles all cards and stores in gameCards
+document.querySelector(".card-front p").innerHTML = gameCards[0].front;//displays the 1st card in card-front
 
 //when nextButton is clicked
+const nextButton = document.getElementById("nextButton");
 nextButton.addEventListener("click", displayValue); //onClick next button, call display value and disply vale on front of card
 
 //display front of card
 const displayValue=()=>{
-    document.getElementById("card-front").innerHTML = gameCards[count].front;//displays the front of the card you are up to in card-front
+    document.querySelector(".card-front p").innerHTML = gameCards[count].front;//displays the front of the card you are up to in card-front
     count++; //increment count each time card is displayed (to get to next card)
 }
 
-card-wrapper.addEventListener("click", flipCard); //when click on card, call flip card function
+document.querySelector(".card-wrapper").addEventListener("click", flipCard); //when click on card, call flip card function
 
 //trigger css to flip and display answer on back
 function flipCard(){
